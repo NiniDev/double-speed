8d2fcfceeed20733393cb3b5b160d06eb9ba5f5e
NiniMine <linantfin@gmail.com>
2022-06-11 12:41:55 +0200

    .

--- main.js
+++ main.js
@@ -2,5 +2,12 @@
 var url = window.location.href;
 // check if it is youtube
 if (url.indexOf("youtube.com") > -1) {
-    alert(document.cookie)
+    let currentSpeed = JSON.parse(sessionStorage.getItem('yt-player-playback-rate')).data
+    if (currentSpeed !== "2") {
+        sessionStorage.setItem('yt-player-playback-rate', JSON.stringify({
+            data: String(2), creation: Date.now()
+        }))
+        console.log("Speed set to 2")
+        location.reload()
+    }
 }
